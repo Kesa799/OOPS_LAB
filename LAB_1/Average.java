ad1207f38400c400dd06333c2c160200bc6b0a61
Nandini Elamurugan <97202957+Kesa799@users.noreply.github.com>
2025-12-22 20:31:07 +0530

    Add files via upload

---
+++ LAB_1/Average.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Average{
+public static void main(String args[]){
+Scanner input = new Scanner(System.in);
+System.out.print("Enter the first number:");
+int a = input.nextInt();
+System.out.print("Enter the second number:");
+int b = input.nextInt();
+System.out.print("Enter the third number:");
+int c = input.nextInt();
+double d = (a+b+c)/3;
+System.out.println("Average: "+d);}}
+
