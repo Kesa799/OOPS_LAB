ecc437ecb49ef43fc9c199f595488496a61cb59c
Nandini Elamurugan <97202957+Kesa799@users.noreply.github.com>
2025-12-29 12:29:07 +0530

    Add files via upload

---
+++ Data_Types/Discount.java
@@ -0,0 +1,11 @@
+class Discount{
+public static void main(String args[]){
+String item = "Dairy Milk Chocolate";
+double amt = 20.0;
+int dp = 5;
+boolean discount = true;
+
+System.out.println("ITEM: "+item);
+System.out.println("AMOUNT: "+amt);
+System.out.println("DISCOUNT %: "+dp);
+System.out.println("DISCOUNT AVAILED: "+discount);}}
