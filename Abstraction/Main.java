d1326d0613c7f1e38dfe858be6fdc07b009b5386
Nandini Elamurugan <97202957+Kesa799@users.noreply.github.com>
2026-04-12 20:42:26 +0530

    Add files via upload

--- Abstraction/Main.java
+++ Abstraction/Main.java
@@ -1,51 +1,23 @@
-// Abstract class
-abstract class Vehicle {
-
-    // Abstract methods
-    abstract void start();
-    abstract void fuelType();
+interface A{
+void meth1();
+void meth2();
 }
 
-// Subclass Car
-class Car extends Vehicle {
-
-    @Override
-    void start() {
-        System.out.println("Car starts with a key or button");
-    }
-
-    @Override
-    void fuelType() {
-        System.out.println("Fuel Type: Petrol/Diesel");
-    }
+class Myclass implements A{
+@Override
+public void meth1(){
+System.out.println("Method 1");
+}
+@Override
+public void meth2(){
+System.out.println("Method 2");
+}
 }
 
-// Subclass ElectricBike
-class ElectricBike extends Vehicle {
-
-    @Override
-    void start() {
-        System.out.println("Electric Bike starts with a switch");
-    }
-
-    @Override
-    void fuelType() {
-        System.out.println("Fuel Type: Electricity");
-    }
+public class Main{
+public static void main(String args[]){
+Myclass m = new Myclass();
+m.meth1();
+m.meth2();
 }
-
-// Main class
-public class Main {
-    public static void main(String[] args) {
-
-        Vehicle v1 = new Car();
-        v1.start();
-        v1.fuelType();
-
-        System.out.println();
-
-        Vehicle v2 = new ElectricBike();
-        v2.start();
-        v2.fuelType();
-    }
 }
